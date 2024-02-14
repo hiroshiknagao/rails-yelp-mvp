bbb11512080f7dd01d845ceecd6cacb173210471
hiroshi nagao <hi_1205@hotmail.com>
2024-02-14 16:58:48 +0900

    yelp mvp stil working on review

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+aca_taco = {name: "Aca Taco", address: "9 Poundary St, Kondon E4 7JC", category: "belgian"}
+bishoom = {name: "Bishoom", address: "10 Jououndary St, Mondon W2 7JE", category: "french"}
+caca_caco = {name: "Caca Caco", address: "98 Bdary St, Rondon N2 3J", category: "japanese"}
+
+[dishoom, pizza_east, aca_taco, bishoom, caca_caco].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
