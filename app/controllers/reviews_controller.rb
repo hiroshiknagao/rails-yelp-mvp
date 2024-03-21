f361a5a068c8f5c4614a784bd9149d787d09efc4
hiroshi nagao <hi_1205@hotmail.com>
2024-03-21 15:56:25 +0900

    moifying review action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,11 @@
 class ReviewsController < ApplicationController
   def new
+    raise
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
+  end
+
+  def create
+    raise
   end
 end
